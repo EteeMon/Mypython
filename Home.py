4069c65cfbff34438f35f7750f6f4ac12b1ba9c1
EteeMon <mionza213@outlook.com>
2023-06-16 14:56:28 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,2 +1,15 @@
 import streamlit as st
 st.header('kairung')
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Versicolor")
+   st.image("./pic/setosa.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./pic/versicolor.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./pic/virginica.jpg")
